c705ca1333412c5e15c821031e9c50e8c1fe67a4
Hung Nguyen <nguyenmanhhungpro15@gmail.com>
2023-08-13 16:47:32 -0400

    update

--- src/app/model/question.model.ts
+++ src/app/model/question.model.ts
@@ -1,7 +1,5 @@
-//Maybe the seperation of Q model and survey model is not needed
 export class Question {
     constructor(
-        // public _id?: number, 
         public Question?: string,
         public OptionOne?: string,
         public OptionTwo?: string,
